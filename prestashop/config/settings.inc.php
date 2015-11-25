b93634df44699dd5b3907c873dceead76dafe840
iveod <208678@supinfo.com>
2015-11-25 17:18:01 +0100

    Initial commit

---
+++ prestashop/config/settings.inc.php
@@ -0,0 +1,16 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', 'xxlboutique');
+define('_DB_USER_', 'root');
+define('_DB_PASSWD_', 'root');
+define('_DB_PREFIX_', 'xxl_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_COOKIE_KEY_', 'jXY3JiCvWG7X6DRwqqZMinCR1UhdMMXvPMv2Xt0YmckfOgpk90ukSAoM');
+define('_COOKIE_IV_', 'Gt0w7qCV');
+define('_PS_CREATION_DATE_', '2015-11-25');
+if (!defined('_PS_VERSION_'))
+	define('_PS_VERSION_', '1.6.1.2');
+define('_RIJNDAEL_KEY_', 'aM3gMVJCSfgKQxLjhk5WfszuQ6mYURas');
+define('_RIJNDAEL_IV_', 'xeKbJyfXd+vVYdRPyyid8Q==');
